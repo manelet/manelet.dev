3c7f85e8465c03ca0a155301cb4d054be67a5d7d
manelgarcia <manelet@gmail.com>
2020-08-21 18:32:18 +0200

    temp: removed gatsby-plugin-feed

--- gatsby-config.js
+++ gatsby-config.js
@@ -36,68 +36,68 @@ module.exports = {
         }
       ]      
     },
-    {
-      resolve: `gatsby-plugin-feed`,
-      options: {
-        query: `
-          {
-            site {
-              siteMetadata {
-                title
-                description
-                siteUrl
-                site_url: siteUrl
-              }
-            }
-          }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map(edge => {
-                return {
-                  ...edge.node.frontmatter,
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ "content:encoded": edge.node.body }]
-                }
-              })
-            },
-            query: `
-              {
-                allMdx (
-                  sort: { order: DESC, fields: [frontmatter___date] }
-                  limit: 1000
-                  filter: { fileAbsolutePath: { regex : "/articles/" } }
-                ) {
-                  edges {
-                    node {
-                      body
-                      excerpt
-                      fields {
-                        slug
-                      }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: "/rss.xml",
-            title: "Your Site's RSS Feed",
-            // optional configuration to insert feed reference in pages:
-            // if `string` is used, it will be used to create RegExp and then test if pathname of
-            // current page satisfied this regular expression;
-            // if not provided or `undefined`, all pages will have feed reference inserted
-            match: "^/articles/",
-            // optional configuration to specify external rss feed, such as feedburner
-            link: "https://feeds.feedburner.com/gatsby/blog",
-          },
+    // {
+    //   resolve: `gatsby-plugin-feed`,
+    //   options: {
+    //     query: `
+    //       {
+    //         site {
+    //           siteMetadata {
+    //             title
+    //             description
+    //             siteUrl
+    //             site_url: siteUrl
+    //           }
+    //         }
+    //       }
+    //     `,
+    //     feeds: [
+    //       {
+    //         serialize: ({ query: { site, allMdx } }) => {
+    //           return allMdx.edges.map(edge => {
+    //             return {
+    //               ...edge.node.frontmatter,
+    //               description: edge.node.excerpt,
+    //               date: edge.node.frontmatter.date,
+    //               url: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
+    //               custom_elements: [{ "content:encoded": edge.node.body }]
+    //             }
+    //           })
+    //         },
+    //         query: `
+    //           {
+    //             allMdx (
+    //               sort: { order: DESC, fields: [frontmatter___date] }
+    //               limit: 1000
+    //               filter: { fileAbsolutePath: { regex : "/articles/" } }
+    //             ) {
+    //               edges {
+    //                 node {
+    //                   body
+    //                   excerpt
+    //                   fields {
+    //                     slug
+    //                   }
+    //                   frontmatter {
+    //                     title
+    //                     date
+    //                   }
+    //                 }
+    //               }
+    //             }
+    //           }
+    //         `,
+    //         output: "/rss.xml",
+    //         title: "Your Site's RSS Feed",
+    //         // optional configuration to insert feed reference in pages:
+    //         // if `string` is used, it will be used to create RegExp and then test if pathname of
+    //         // current page satisfied this regular expression;
+    //         // if not provided or `undefined`, all pages will have feed reference inserted
+    //         match: "^/articles/",
+    //         // optional configuration to specify external rss feed, such as feedburner
+    //         link: "https://feeds.feedburner.com/gatsby/blog",
+    //       },
         ],
       },
     }    
