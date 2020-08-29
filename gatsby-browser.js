75894ef89b33f52298d706ae8651c2fa405e3635
manelgarcia <manelet@gmail.com>
2020-08-29 10:13:06 +0200

    fix

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,7 +2,6 @@ import wrapRoot from './gatsby/wrap-root-element'
 import wrapPage from './gatsby/wrap-page-element'
 import updateScroll from './gatsby/should-update-scroll'
 
-import './node_modules/@fortawesome/fontawesome-free/css/all.min.css'
 import './src/styles/main.css'
 
 export const wrapRootElement = wrapRoot
