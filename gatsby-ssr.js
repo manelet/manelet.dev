1a479a5363f1c2c7aa2e111fd9293ddfed585afe
manelgarcia <manelet@gmail.com>
2020-08-29 19:25:02 +0200

    feat: added html attribute lang

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -4,6 +4,9 @@ import wrapRoot from './gatsby/wrap-root-element'
 import wrapPage from './gatsby/wrap-page-element'
 import { DarkThemeScript } from './src/lib/dark-theme'
 
-export const onRenderBody = ({ setPreBodyComponents }) => setPreBodyComponents(<DarkThemeScript />)
+export const onRenderBody = ({ setHtmlAttributes, setPreBodyComponents }) => {
+  setPreBodyComponents(<DarkThemeScript />)
+  setHtmlAttributes({ lang: 'en' })
+}
 export const wrapRootElement = wrapRoot
 export const wrapPageElement = wrapPage
