0818ad89dee9c9003269504e794842c7191a0987
manelgarcia <manelet@gmail.com>
2020-08-31 07:18:25 +0200

    fix: svg colors nav

--- src/components/ThemeToggle.js
+++ src/components/ThemeToggle.js
@@ -35,6 +35,8 @@ const ThemeToggle = () => {
     toggleTheme(isDark ? 'light' : 'dark')
   }, [toggleTheme, isDark])
 
+  const color = isDark ? '#fff' : '#2d3748'
+
   return (
     <div
       onClick={updateTheme}
@@ -42,50 +44,40 @@ const ThemeToggle = () => {
       style={{ transformOrigin: 'center center', transformStyle: 'preserve-3d', transformBox: 'fill-box' }}      
     >
       <AnimatePresence exitBeforeEnter initial={false}>
-        {isDark && (
-          <motion.svg
-            variants={variants}
-            key='toggle-dark'
-            initial='hide'
-            animate='show'
-            exit='hide'          
-            fill="none"
-            height="18" 
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            viewBox="0 0 24 24"
-            width="18"
-          >
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
-          </motion.svg>          
-        )}
-        {!isDark && (
-          <motion.svg
-            fill="none"
-            height="18"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2" 
-            viewBox="0 0 24 24"
-            width="18"
-            variants={variants}
-            key='toggle-light'
-            initial='hide'
-            animate='show'
-            exit='hide' 
-          >
-            <circle cx="12" cy="12" r="5" />
-            <line x1="12" x2="12" y1="1" y2="3" />
-            <line x1="12" x2="12" y1="21" y2="23" />
-            <line x1="4.22" x2="5.64" y1="4.22" y2="5.64" />
-            <line x1="18.36" x2="19.78" y1="18.36" y2="19.78" />
-            <line x1="1" x2="3" y1="12" y2="12" />
-            <line x1="21" x2="23" y1="12" y2="12" />
-            <line x1="4.22" x2="5.64" y1="19.78" y2="18.36" />
-            <line x1="18.36" x2="19.78" y1="5.64" y2="4.22" />
-          </motion.svg>          
-        )}        
+        <motion.svg
+          variants={variants}
+          key={`toggle-${isDark ? 'dark' : 'light'}`}
+          initial='hide'
+          animate='show'
+          exit='hide'          
+          fill="none"
+          height="18" 
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth="2"
+          viewBox="0 0 24 24"
+          width="18"
+          style={{ fill: color, stroke: color }}
+        >
+          {isDark
+            ? (
+              <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" />
+            )
+            : (
+              <>
+                <circle cx="12" cy="12" r="5" />
+                <line x1="12" x2="12" y1="1" y2="3" />
+                <line x1="12" x2="12" y1="21" y2="23" />
+                <line x1="4.22" x2="5.64" y1="4.22" y2="5.64" />
+                <line x1="18.36" x2="19.78" y1="18.36" y2="19.78" />
+                <line x1="1" x2="3" y1="12" y2="12" />
+                <line x1="21" x2="23" y1="12" y2="12" />
+                <line x1="4.22" x2="5.64" y1="19.78" y2="18.36" />
+                <line x1="18.36" x2="19.78" y1="5.64" y2="4.22" />
+              </>
+            )
+          }
+        </motion.svg>          
       </AnimatePresence>
     </div>
   )
--- src/lib/navScroll.js
+++ src/lib/navScroll.js
@@ -1,40 +1,42 @@
+function paint (svgs, color) {
+  svgs.forEach(svg => {
+    if (svg) {
+      svg.style.fill = color
+      svg.style.stroke = color
+    }
+  })   
+}
+
 export default (nav, navInner, splash, isDark, isHome) => {
   const { height: navHeight } = nav.getBoundingClientRect()
   const { height: splashHeight } = splash.getBoundingClientRect()
   const svgs = [...nav.querySelectorAll('.icons .icon svg'), nav.querySelector('.theme-toggle svg')]
-
+console.log('ntro navscroll', svgs);
   if (window.scrollY > (splashHeight - navHeight)) {
     // navInner.style.display = 'flex'
-    console.log('svgs', svgs);
 
     if (!isDark) {
       nav.classList.add('text-gray-800')
       nav.classList.remove('text-white')
-      svgs.forEach(svg => {
-        svg.style.fill = '#2d3748'
-        svg.style.stroke = '#2d3748'
-      })
+      paint(svgs, '#2d3748')
     } else {
       nav.classList.remove('text-gray-800')
       nav.classList.add('text-white')
-      svgs.forEach(svg => {
-        svg.style.fill = '#fff'
-        svg.style.stroke = '#fff'
-      })      
+      paint(svgs, '#fff')
     }
 
     nav.classList.remove('home')
   } else {
     // navInner.style.display = 'none'
     nav.classList.add('home')
+    console.log('ntro navscroll2');
 
     if (!isDark) {
+      console.log('ntro navscroll3');
+
       nav.classList.remove('text-gray-800')
       nav.classList.add('text-white')  
-      svgs.forEach(svg => {
-        svg.style.fill = '#fff'
-        svg.style.stroke = '#fff'
-      })      
+      paint(svgs, '#fff')
     }
   }
 }
