d97d88909cfbb20b7b53cce4375a183bb3aa464b
manelgarcia <manelet@gmail.com>
2020-10-29 12:02:59 +0100

    fix: scroll on article subtitle (hash)

--- gatsby/should-update-scroll.js
+++ gatsby/should-update-scroll.js
@@ -1,30 +1,26 @@
 export const duration = 500
 
-const shouldUpdateScroll = props => {
-  const { prevRouterProps, routerProps, getSavedScrollPosition } = props
+const shouldUpdateScroll = ({ prevRouterProps, routerProps, getSavedScrollPosition }) => {
+  const projectsFromOutside = routerProps.location.pathname.includes('/projects') && prevRouterProps.location.pathname.includes('/projects')
+  const isHash = routerProps.location.hash !== '' && !routerProps.location.action
 
-  if (
-    routerProps.location.pathname.includes('/projects') &&
-    prevRouterProps.location.pathname.includes('/projects')
-  ) {
+  if (projectsFromOutside || isHash) {
     return false
   }
 
+
   if (routerProps.location.action === "PUSH") {
-    if (routerProps.location.pathname.includes('/projects')) {
-      window.scrollTo(0, 0)
-    } else {
-      window.setTimeout(() => {
-        window.scrollTo(0, 0)
-      }, duration)
-    }
+    window.setTimeout(() => window.scrollTo(0, 0), duration)    
+    // if (routerProps.location.pathname.includes('/projects')) {
+    //   window.scrollTo(0, 0)
+    // } else {
+    //   window.setTimeout(() => {
+    //     window.scrollTo(0, 0)
+    //   }, duration)
+    // }
   } else {
     const savedPosition = getSavedScrollPosition(routerProps.location)
-
-    window.setTimeout(
-      () => window.scrollTo(...(savedPosition || [0, 0])),
-      duration
-    )
+    window.setTimeout(() => window.scrollTo(...(savedPosition || [0, 0])), duration)
   }
   return false
 }
