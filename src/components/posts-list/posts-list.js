e8e32b0edc4a3d4f0611678da4c4ea97814747e6
manelgarcia <manelet@gmail.com>
2020-10-31 11:12:58 +0100

    enhance: better latests posts on homepage

--- src/components/posts-list/posts-list.js
+++ src/components/posts-list/posts-list.js
@@ -1,56 +1,74 @@
-import React, { useCallback } from 'react'
+import React from 'react'
 import { Link, navigate } from 'gatsby'
 import { motion } from 'framer-motion'
 import { MDXRenderer } from 'gatsby-plugin-mdx'
+import dayjs from 'dayjs'
+import relativeTime from 'dayjs/plugin/relativeTime'
 
 import useWindow from '../../hooks/useWindow'
 
 import './posts-list.css'
 
-const createMarkup = __html => ({ __html })
+// const createMarkup = __html => ({ __html })
 const boxShadow = '0 20px 25px -5px rgba(0, 0, 0, 0.1), 0 10px 10px -5px rgba(0, 0, 0, 0.04)'
+
+dayjs.extend(relativeTime)
 
 const PostsList = ({ title, posts = [] }) => {
   const { isMobile } = useWindow()
-  const goTo = useCallback(e => {
+  const goTo = url => e => {
     e.preventDefault()
     e.stopPropagation()
-    const url = e.currentTarget.getAttribute('data-path')
     navigate(url)
-  }, [])
+  }
 
-  const hoverAnimation = !isMobile ? { scale: 1.05, boxShadow }: null
+  const goToArticles = () => navigate('/articles')
+
+  const hoverAnimation = !isMobile ? { scale: 1.15, boxShadow, borderRadius: '5px' }: null
 
   return (
     <div id='home-articles' className="posts-list cont">
       <div className="cont-inner flex-col">
         {title && (
-          <h2 className='font-bold text-3xl'>{title}</h2>
+          <h2 className='font-bold text-3xl my-10'>{title}</h2>
         )}
 
         <div className='posts-wrapper'>
-          {posts.length && posts.map(({ post }) => (
-            <motion.div
-              whileHover={hoverAnimation}
-              key={`postlist-${post.fields.slug}`}
-              className="post"
-              data-path={post.fields.slug}
-              onClick={goTo}
-            >
-              <div className='post-inner'>
-                <h3 className='text-2xl'>
-                  <Link to={post.fields.slug}>
-                    {post.frontmatter.title}
-                  </Link>
-                </h3>
-                <MDXRenderer onlyExcerpt={true}>
-                  {post.body}
-                </MDXRenderer>                
-                {/* <div dangerouslySetInnerHTML={createMarkup(post.excerpt)} /> */}
-              </div>
-            </motion.div>
-          ))}
+          {posts.length && posts.map(({ post }) => {
+            const dateObj = dayjs(post.frontmatter.date, 'MMMM DD, YYYY')
+            const numberOfDaysSincePublication = dayjs().diff(dateObj, 'day')
+            
+            return (
+              <motion.div
+                whileHover={hoverAnimation}
+                key={`postlist-${post.fields.slug}`}
+                className="post"
+                onClick={goTo(post.fields.slug)}
+              >
+                <div className='post-inner flex flex-col-reverse md:flex-row w-full'>
+                  <div className='md:flex-shrink-0 w-full md:w-48 text-gray-500'>
+                    {numberOfDaysSincePublication <= 60 ? dateObj.fromNow() : dateObj.format('DD/MM/YY')}
+                  </div>
+                  <div>
+                    <h3 className='text-2xl font-bold'>
+                      <Link to={post.fields.slug}>
+                        {post.frontmatter.title}
+                      </Link>
+                    </h3>
+
+                    <MDXRenderer onlyExcerpt={true}>
+                      {post.body}
+                    </MDXRenderer>                
+                  </div>
+                </div>
+              </motion.div>
+            )}
+          )}
         </div>
+
+        <button onClick={goToArticles}>
+          View all
+        </button>
       </div>
     </div>
   )
