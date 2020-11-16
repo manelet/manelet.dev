8cf3edbbf99b7d9d3d0659d96e836d39c995b4ec
manelgarcia <manelet@gmail.com>
2020-11-16 18:35:42 +0100

    fix: multiple career comp bug fixes

--- src/components/about/career.js
+++ src/components/about/career.js
@@ -1,19 +1,27 @@
 import React from 'react'
+import dayjs from 'dayjs'
 
 import careerPath from '../../../static/career.json'
 
 import './career.css'
 
-const Career = ({ location }) => {
+const Career = () => {
   return (
     <div className='careers'>
       <div className="careers__inner">
         {careerPath.map((job, i) => (
           <div className='career' key={`job-${i}`}>
             <div className="career__role">
-              <div className="font-bold text-xl">
-                {job.role}
-              </div>
+              {job.roles.map(({ role, startedAt, endedAt }) => (
+                <div className='career__role__detail'>
+                  <div className="font-bold text-xl">
+                    {role}
+                  </div>
+                  <p>
+                    {job.company} <span>{dayjs(startedAt).format('MMM YYYY')} - {!endedAt ? 'Present' : dayjs(endedAt).format('MMM YYYY')}</span>
+                  </p>                
+                </div>
+              ))}
             </div>
             <div className="career__sep" />
             <div className="career__detail">
