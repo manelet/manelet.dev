882e3048063844064b4e2cbec4a7443bb93b2660
manelgarcia <manelet@gmail.com>
2020-11-24 08:23:59 +0100

    fix: window height in mobile browsers not real 100vh

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,10 +2,10 @@ import React from 'react'
 
 import wrapRoot from './gatsby/wrap-root-element'
 import wrapPage from './gatsby/wrap-page-element'
-import { DarkThemeScript } from './src/lib/dark-theme'
+import SSRScripts from './src/lib/ssr-scripts'
 
 export const onRenderBody = ({ setHtmlAttributes, setPreBodyComponents }) => {
-  setPreBodyComponents(<DarkThemeScript />)
+  setPreBodyComponents(<SSRScripts />)
   setHtmlAttributes({ lang: 'en' })
 }
 export const wrapRootElement = wrapRoot
